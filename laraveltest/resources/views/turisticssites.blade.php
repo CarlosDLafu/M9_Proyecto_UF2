ea2d140240067e407ed158ed92219a82ece2eab8
Carlos Lafuente <carlos.lafuente@insbaixcamp.com>
2024-01-16 20:26:16 +0100

    Juntando ramas

---
+++ laraveltest/resources/views/turisticssites.blade.php
@@ -0,0 +1,83 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Turistic Sites') }}
+        </h2>
+    </x-slot>
+
+    <nav class="grid" 
+    style="grid-template-columns: repeat(3, minmax(0, 1fr)); padding: 2.5rem; gap: 1.25rem;">
+        <!--Card 1-->
+        <div class="rounded overflow-hidden shadow-lg" style="height: 700px;">
+            <img class="w-full" style="height: 100%;" src="/pintura-lago-montana-montana-al-fondo_188544-9126.avif" alt="Mountain">
+            <div class="px-6 py-4" style="top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  position: sticky;">
+                <div class="text-center mb-2" style="position: sticky;
+  top: 15%; background-color: #00000040">Bali<br><span style="font-size:30px;">Kuta</span></div>
+                <button class="text-white text-center" style="position: absolute;
+  bottom: 20%; left: 40%; background-color: orange">
+                Ver más    
+            </p>
+            </div>
+        </div>
+        <!--Card 1-->
+        <div class="rounded overflow-hidden shadow-lg" style="height: 700px;">
+            <img class="w-full" style="height: 100%;" src="/pintura-lago-montana-montana-al-fondo_188544-9126.avif" alt="Mountain">
+            <div class="px-6 py-4" style="top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  position: sticky;">
+                <div class="text-center mb-2" style="position: sticky;
+  top: 15%; background-color: #00000040">Bali<br><span style="font-size:30px;">Kuta</span></div>
+                <button class="text-white text-center" style="position: absolute;
+  bottom: 20%; left: 40%; background-color: orange">
+                Ver más    
+            </p>
+            </div>
+        </div>
+        <!--Card 1-->
+        <div class="rounded overflow-hidden shadow-lg" style="height: 700px;">
+            <img class="w-full" style="height: 100%;" src="/pintura-lago-montana-montana-al-fondo_188544-9126.avif" alt="Mountain">
+            <div class="px-6 py-4" style="top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  position: sticky;">
+                <div class="text-center mb-2" style="position: sticky;
+  top: 15%; background-color: #00000040">Bali<br><span style="font-size:30px;">Kuta</span></div>
+                <button class="text-white text-center" style="position: absolute;
+  bottom: 20%; left: 40%; background-color: orange">
+                Ver más    
+            </p>
+            </div>
+        </div>
+        <!--Card 1-->
+        <div class="rounded overflow-hidden shadow-lg" style="height: 700px;">
+            <img class="w-full" style="height: 100%;" src="/pintura-lago-montana-montana-al-fondo_188544-9126.avif" alt="Mountain">
+            <div class="px-6 py-4" style="top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  position: sticky;">
+                <div class="text-center mb-2" style="position: sticky;
+  top: 15%; background-color: #00000040">Bali<br><span style="font-size:30px;">Kuta</span></div>
+                <button class="text-white text-center" style="position: absolute;
+  bottom: 20%; left: 40%; background-color: orange">
+                Ver más    
+            </p>
+            </div>
+        </div>
+    </nav>
+</x-app-layout>
