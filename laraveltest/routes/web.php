180b4b2278484fc5a6f0aa09e076e254a0d57295
Carlos Lafuente <carlos.lafuente@insbaixcamp.com>
2024-01-16 17:53:16 +0100

    Plantilla del Layout

--- laraveltest/routes/web.php
+++ laraveltest/routes/web.php
@@ -22,6 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/turisticssites', function () {
+    return view('turisticssites');
+})->middleware(['auth', 'verified'])->name('turisticssites');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
